4e9a3c7e5da03c479f279d92cdb3b69ce74fb648
Lefford <lefford.girigori@gmail.com>
2012-03-05 13:04:37 +0100

    Added named urls

--- hotels/urls.py
+++ hotels/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
-from hotels.views import test_view, home_view
+from hotels.views import test_view, home_view, result_view
 
 urlpatterns = patterns('',
-     url(r'^$', home_view, name='home-view'),
-     url(r'^test-form/$', test_view, name='test-form') 
+     url(r'^$', home_view, name='home'),
+     url(r'^test-form/$', test_view, name='choice-form'),
+     url(r'^success/(\w+)/$', result_view, name='result'),
 )
