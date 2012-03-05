316e61687251cdf6e519fb76974a6dd424fec1b7
Lefford <lefford.girigori@gmail.com>
2012-03-05 13:03:40 +0100

    Fixed form validation

--- hotels/forms.py
+++ hotels/forms.py
@@ -4,13 +4,23 @@ from hotels.models import City, Hotel
 class HotelForm(forms.Form):
 	
 	def __init__(self, *args, **kwargs):
+		request = kwargs.pop('request', None)
 		super(HotelForm, self).__init__(*args, **kwargs)
 
-		# get initial value
-		country_values = [(i.code, i.name) for i in City.objects.all()]
-		city = City.objects.get(code='AMS')
+		# get the city for hotels
+		city_code = request.POST.get('cities', None)
+		try:
+			city = City.objects.get(code=city_code)
+		except City.DoesNotExist:
+			city = City.objects.get(code='AMS')
+		
+		# selectbox value
 		hotel_values = [(i.code, i.name) for i in Hotel.objects.filter(city=city)]
+		country_values = [(i.code, i.name) for i in City.objects.all()]
 
 		# set field choices
-		self.fields['city'] = forms.ChoiceField(choices=country_values)
+		self.fields['cities'] = forms.ChoiceField(choices=country_values)
 		self.fields['hotels'] = forms.ChoiceField(choices=hotel_values)
+		
+		self.fields['cities'].widget.attrs['class'] = 'chzn-select'
+		self.fields['hotels'].widget.attrs['class'] = 'chzn-select'
